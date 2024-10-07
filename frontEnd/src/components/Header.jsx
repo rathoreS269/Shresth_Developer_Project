da9615a0fd6b9d8834944f48d19b6b28342dc2bf
shruti rathore <rathoreshruti269@gmail.com>
2024-10-07 15:00:22 +0530

    fetched user avatar and displayed

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 function Header() {
+    const { currentUser } = useSelector((state) => state.user);
+    console.log('current user is', currentUser);
+    if (currentUser) {
+      console.log('name is', currentUser.email);
+      console.log('avatar url is', currentUser.avatar);
+    }
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -24,8 +31,13 @@ function Header() {
        <Link to='/about'>
        <li className=' hidden sm:inline text-slate-700 hover:underline'>About</li>
        </Link>
-       <Link to='/sign-in'>
-       <li className='  text-slate-700 hover:underline'>SignIn</li>
+       <Link to='/profile'>
+       {currentUser ? (
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+       
        </Link>
       </ul>
         </div>
--- frontEnd/src/redux/user/userSlice.js
+++ frontEnd/src/redux/user/userSlice.js
@@ -1,29 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    currentUser : null,
+    currentUser: null,
     error: null,
     loading: false,
 };
 
 const userSlice = createSlice({
-    name : 'user',
+    name: 'user',
     initialState,
-    reducers:{
-        signInStart: (state) =>{
+    reducers: {
+        signInStart: (state) => {
             state.loading = true;
         },
-        signInSuccess: (state, action) =>{
+        signInSuccess: (state, action) => {
             state.currentUser = action.payload;
             state.loading = false;
             state.error = null;
         },
-        signInFailure: (state, action) =>{
+        signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
         }
     }
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
 export default userSlice.reducer;
